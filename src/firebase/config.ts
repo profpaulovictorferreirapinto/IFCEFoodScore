4b912a02dde9899602aef58586a61f662c0432a9
prof_paulovictor <paulo.victor@ifce.edu.br>
2026-04-16 12:00:16 -0300

    Delete src/firebase directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-9964985319-d2516",
-  "appId": "1:496033450630:web:51f692ab26c24130a51f07",
-  "apiKey": "AIzaSyD1hXriR4gIuBXDIfZDWAddHp1hNievcUI",
-  "authDomain": "studio-9964985319-d2516.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "496033450630"
-};
